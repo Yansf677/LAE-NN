621e815b3ebf3206723aa71f872fc46a5f1389a3
S. Yan <38400910+Yansf677@users.noreply.github.com>
2019-06-02 22:22:22 +0800

    Update data_divide.m

--- data_divide.m
+++ data_divide.m
@@ -1,26 +1,31 @@
 clc
 clear
-%% data for training
+%% prepare training data
 load TEdata.mat
-all_x = []; all_y = [];
-train_x = [];train_y = [];
-index = [1 2 4 5 6 7 21];class = size(index,2);
+all_x = [];
+all_y = [];
+train_x = [];
+train_y = [];
+index = [1 2 4 5 6 7 21]
+class = size(index, 2)
 for i = 1:class
-    y = 0.*ones(480,class);y(:,i) = ones(480,1);
-    train_x = [train_x;TE_480(:,:,index(i))];
-    train_y = [train_y;y];
+    y = 0.*ones(480, class);
+    y(:, i) = ones(480, 1);
+    train_x = [train_x; TE_480(:, :, index(i))];
+    train_y = [train_y; y];
 end
+train_x = train_x'; 
+[train_x settings] = mapminmax(train_x, 0, 1);
+train_x = train_x'; 
+train_data = [train_x train_y];
 
-train_x = train_x'; [train_x settings] = mapminmax(train_x,0,1);
-train_x = train_x'; train_data = [train_x train_y];
-
-%% data for testing
+%% prepare testing data
 test_x = [];
 for i = 1:class
-    test_x = [test_x;TE_960(1:960,:,index(i))];
+    test_x = [test_x; TE_960(1:960, :, index(i))];
 end
 test_x = test_x';
-test_x = mapminmax.apply(test_x,settings);
+test_x = mapminmax.apply(test_x, settings);
 test_x = test_x';
 clear class
 clear d00
